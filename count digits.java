1a9225c3e9af34d5576d07f213dd688811bc1a6d
Ankit Mourya <113794564+Ankit-69-m@users.noreply.github.com>
2023-09-14 23:27:52 +0530

    Add files via upload

---
+++ count digits.java
@@ -0,0 +1,17 @@
+class Solution{
+    public static int evenlyDivides(int N) {
+        int count = 0;
+        int originalN = N;
+
+        while (N > 0) {
+            int digit = N % 10;
+            if (digit != 0 && originalN % digit == 0) {
+                count++;
+            }
+            N /= 10;
+        }
+
+        return count;
+    }
+
+}
